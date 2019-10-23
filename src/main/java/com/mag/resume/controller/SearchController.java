bffcc7fbbfd3abd5ce1d020e5bdada6d9150a4fb
Artem <Artem@DESKTOP-H8KHPGU>
2019-10-23 17:07:13 +0300

    Structure of project, controller, filter,listener and service

---
+++ src/main/java/com/mag/resume/controller/SearchController.java
@@ -0,0 +1,47 @@
+package com.mag.resume.controller;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mag.resume.service.NameService;
+
+
+
+
+@WebServlet("/search")
+public class SearchController extends HttpServlet{
+	private static final long serialVersionUID = 2847786613235117603L;
+	private static final Logger LOGGER = LoggerFactory.getLogger(SearchController.class);
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		LOGGER.debug("doGet search");
+		req.getRequestDispatcher("/WEB-INF/JSP/search-form.jsp").forward(req, resp);
+	}
+	
+	private boolean isValid(String name) {
+		return name != null && name.trim().length() != 0;
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		LOGGER.debug("doPost search: {}", req.getParameterMap());
+		String name = req.getParameter("query");
+		if(!isValid(name)) {
+			req.setAttribute("invalid", Boolean.TRUE);
+			req.getRequestDispatcher("/WEB-INF/JSP/search-form.jsp").forward(req, resp);
+			return;
+		}
+		String result = NameService.getInstance().convertName(name);
+		LOGGER.debug(result);
+		req.setAttribute("name", result);
+		req.getRequestDispatcher("/WEB-INF/JSP/search-result.jsp").forward(req, resp);
+	}
+}
---
+++ src/main/java/com/mag/resume/listener/ApplicationListener.java
@@ -0,0 +1,21 @@
+package com.mag.resume.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebListener
+public class ApplicationListener implements ServletContextListener {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationListener.class);
+	public void contextInitialized(ServletContextEvent sce) {
+		LOGGER.info("Application started");
+	}
+
+	public void contextDestroyed(ServletContextEvent sce) {
+		LOGGER.info("Application destroyed");
+	}
+
+}
